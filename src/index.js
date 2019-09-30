b6692b04b681221edd2dbb488af7dbc3746bc7ab
Russell McWhae <russell.mcwhae@gmail.com>
2019-09-30 14:47:54 -0600

    Working boat and explosion sprite

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Phaser from 'phaser';
 import logoImg from './assets/logo.png';
-import splashImg from './assets/Preview.png';
+import greenBoatImg from './assets/green_battleship_sprite.png';
+import explosionImg from './assets/explosion.png';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
@@ -15,22 +16,51 @@ const config = {
   },
   scale: {
     // mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH,
+    autoCenter: Phaser.Scale.CENTER_HORIZONTALLY
   }
 };
 const game = new Phaser.Game(config);
 function preload() {
   // this.load.image('splash', splashImg);
+  this.load.spritesheet('greenBoat', greenBoatImg, {
+    frameWidth: 66,
+    frameHeight: 113
+  });
+  this.load.spritesheet('boom', explosionImg, {
+    frameWidth: 64,
+    frameHeight: 64,
+    endFrame: 23
+  });
 }
 function create() {
-  // const logo = this.add.image(500, 300, 'splash');
-  var graphics = this.add.graphics();
-
-  this.add.text(200 - 320 / 2, 0, 'Your Ships', { font: '24pt "Inconsolata"', fill: 'green' });
-  this.add.text(600 - 320 / 2, 0, 'Opponent', { font: '24pt "Inconsolata"', fill: 'green' });
+  this.add.text(200 - 320 / 2, 0, 'Your Ships', {
+    font: '24pt "Inconsolata"',
+    fill: 'green'
+  });
+  this.add.text(600 - 320 / 2, 0, 'Opponent', {
+    font: '24pt "Inconsolata"',
+    fill: 'green'
+  });
 
   const playerBoard = this.add.grid(200, 200, 320, 320, 40, 40, 0x057605);
   const opponentBoard = this.add.grid(600, 200, 320, 320, 40, 40, 0x057605);
+
+  const boat = this.add.sprite(100, 200, 'greenBoat');
+  var config = {
+    key: 'explode',
+    frames: this.anims.generateFrameNumbers('boom', {
+      start: 0,
+      end: 23,
+      first: 23
+    }),
+    frameRate: 20
+  };
+
+  this.anims.create(config);
+
+  const boom = this.add.sprite(100, 300, 'boom');
+
+  boom.anims.play('explode');
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
