462debf79d8ad895ba6d14147c62667f043cb5a3
Russell McWhae <russell.mcwhae@gmail.com>
2019-09-30 17:11:53 -0600

    Working ship display, except for horizontal

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ const config = {
 const gridDimensions = {
   singleSquareLength: 60,
   gridRows: 6
-}
+} // e.g. we have a <gridRows> x <gridRows> square grid where each square has a width and height of <singleSquareLength>
 const game = new Phaser.Game(config);
 function preload() {
   // this.load.image('splash', splashImg);
@@ -50,18 +50,35 @@ function create() {
   const playerBoard = this.add.grid(200, 230, boardLength, boardLength, gridDimensions.singleSquareLength, gridDimensions.singleSquareLength, 0x057605);
   const opponentBoard = this.add.grid(650, 230, boardLength, boardLength, gridDimensions.singleSquareLength, gridDimensions.singleSquareLength, 0x057605);
 
-  const boat = this.add.sprite(46, 230, 'greenBoat');
+  let boat1, boat2, boat3, boat4, boat5;
+  // const boat3 = this.add.sprite(46, 230, 'greenBoat');
+  // const boat4 = this.add.sprite(46, 230, 'greenBoat');
+  // const boat5 = this.add.sprite(46, 230, 'greenBoat');
+  // boat1 = this.add.sprite(206, 230, 'greenBoat');
+  // boat2 = this.add.sprite(46, 230, 'greenBoat');
   
-  boat.rotation = Math.PI / 2; // rotate 90 degrees
-  boat.x += gridDimensions.singleSquareLength / 2;
-  boat.y -= gridDimensions.singleSquareLength / 2;
+  // boat1.rotation = Math.PI / 2; // rotate 90 degrees
+  // boat1.angle = 90;
+  // boat1.y -= gridDimensions.singleSquareLength / 2;
 
-  Phaser.Actions.GridAlign(boat, {
-    cellWidth: 32,
-    cellHeight: 32,
-    x: 100,
-    y: 100
-  });
+  let playerOneShips = [
+    { x: 0, y: 0, size: 2, hits: 0, horizontal: false },
+    { x: 5, y: 4, size: 2, hits: 0, horizontal: true },
+    { x: 2, y: 3, size: 2, hits: 0, horizontal: true },
+    { x: 4, y: 1, size: 2, hits: 0, horizontal: false },
+    { x: 2, y: 5, size: 2, hits: 0, horizontal: true }
+  ];
+
+  renderShips(this, playerOneShips);
+
+  // Phaser.Actions.GridAlign([boat1, boat2], {
+  //   width: gridDimensions.gridRows,
+  //   height: gridDimensions.gridRows,
+  //   cellWidth: gridDimensions.singleSquareLength,
+  //   cellHeight: gridDimensions.singleSquareLength,
+  //   x: 100,
+  //   y: 100
+  // });
 
   var config = {
     key: 'explode',
@@ -80,17 +97,16 @@ function create() {
   boom.anims.play('explode');
 }
 
-let playerOneShips = [
-  { x: 5, y: 4, size: 2, hits: 0, horizontal: false },
-  { x: 5, y: 4, size: 2, hits: 0, horizontal: true },
-  { x: 5, y: 4, size: 2, hits: 0, horizontal: true },
-  { x: 5, y: 4, size: 2, hits: 0, horizontal: false },
-  { x: 5, y: 4, size: 2, hits: 0, horizontal: false }
-];
 
-
-const renderShips = function(shipsArray) {
-  shipsArray.foreach();
+const renderShips = function(game, shipsArray) {
+  shipsArray.forEach(ship => {
+    game.add.sprite(50 + ship.x * gridDimensions.singleSquareLength, 110 + ship.y * gridDimensions.singleSquareLength, 'greenBoat')
+    if (ship.horizontal) {
+      console.log(ship);
+      ship.angle = 90;
+      ship.y -= gridDimensions.singleSquareLength / 2 
+  }
+  });
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
