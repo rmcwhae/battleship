e1c6fd22bdc3fdd543f7c6ea7527a5d17c77ebdf
Mike Chui <mikel.k.khui@gmail.com>
2019-10-04 10:49:59 -0600

    updated gameState to include shots to track shots taken to render

--- src/sample0.js
+++ src/sample0.js
@@ -2,8 +2,8 @@
 module.exports = {
   gameState: {
     player_id: '123qwe456rty789uio',
-    boards: { own: 
-              { a: [0,0,0,0,0,0],
+    shots: { own: 
+              { a: [1,1,0,0,0,1],
                 b: [0,0,0,0,0,0],
                 c: [0,0,0,0,0,0],
                 d: [0,0,0,0,0,0],
@@ -15,23 +15,40 @@ module.exports = {
                 b: [0,0,0,0,0,0],
                 c: [0,0,0,0,0,0],
                 d: [0,0,0,0,0,0],
-                e: [0,0,0,0,0,0],
-                f: [0,0,0,0,0,0]
+                e: [0,0,1,0,0,0],
+                f: [1,1,0,0,0,0]
+              }
+            },
+    boards: { own: 
+              { a: [1,1,0,0,0,0],
+                b: [0,1,1,0,0,0],
+                c: [0,0,1,0,0,0],
+                d: [0,0,1,0,0,0],
+                e: [0,0,0,1,1,0],
+                f: [0,0,0,0,1,1]
+              },
+            opponent:
+              { a: [0,0,0,0,1,1],
+                b: [0,0,0,1,1,0],
+                c: [0,0,1,0,0,0],
+                d: [0,0,1,0,0,0],
+                e: [0,1,1,0,0,0],
+                f: [1,1,0,0,0,0]
               }
             },
     ships:  { own:
-                [ { row: 'a', col: 1, size: 2, sunk: false, horizontal: true},
-                  { row: 'b', col: 2, size: 2, sunk: false, horizontal: true},
-                  { row: 'c', col: 3, size: 2, sunk: false, horizontal: false},
-                  { row: 'e', col: 4, size: 2, sunk: false, horizontal: true},
-                  { row: 'f', col: 5, size: 2, sunk: false, horizontal: true},
+                [ { row: 'a', col: 1, size: 2, sunk: true, hit: true, horizontal: true},
+                  { row: 'b', col: 2, size: 2, sunk: false, hit: false, horizontal: true},
+                  { row: 'c', col: 3, size: 2, sunk: false, hit: false, horizontal: false},
+                  { row: 'e', col: 4, size: 2, sunk: false, hit: false, horizontal: true},
+                  { row: 'f', col: 5, size: 2, sunk: false, hit: false, horizontal: true},
                 ],
               opponent:
-                [ { row: 'a', col: 4, size: 2, sunk: false, horizontal: true},
-                  { row: 'b', col: 3, size: 2, sunk: false, horizontal: true},
-                  { row: 'c', col: 3, size: 2, sunk: false, horizontal: false},
-                  { row: 'e', col: 2, size: 2, sunk: false, horizontal: true},
-                  { row: 'f', col: 1, size: 2, sunk: false, horizontal: true},
+                [ { row: 'a', col: 5, size: 2, sunk: false, hit: false, horizontal: true},
+                  { row: 'b', col: 4, size: 2, sunk: false, hit: false, horizontal: true},
+                  { row: 'c', col: 3, size: 2, sunk: false, hit: false, horizontal: false},
+                  { row: 'e', col: 2, size: 2, sunk: false, hit: true, horizontal: true},
+                  { row: 'f', col: 1, size: 2, sunk: true, hit: true, horizontal: true},
                 ]
             },
     turn: { player: 'server',
--- src/sample1.js
+++ src/sample1.js
@@ -1,42 +1,59 @@
 // Data structure
 module.exports = {
   gameState: {
-    player_id: 'sample1',
-    boards: { own: 
-              { a: [0,0,0,0,0,1],
-                b: [0,0,0,0,0,0],
+    player_id: '123qwe456rty789uio',
+    shots: { own: 
+              { a: [1,1,0,0,0,1],
+                b: [0,1,0,0,0,0],
                 c: [0,0,0,0,0,0],
                 d: [0,0,0,0,0,0],
                 e: [0,0,0,0,0,0],
-                f: [1,0,0,0,0,0]
+                f: [0,0,0,0,0,0]
               },
             opponent:
-              { a: [1,0,0,0,0,0],
+              { a: [0,0,0,0,0,0],
                 b: [0,0,0,0,0,0],
                 c: [0,0,0,0,0,0],
-                d: [0,0,0,0,0,0],
-                e: [0,0,0,0,0,0],
-                f: [0,0,0,0,0,1]
+                d: [0,0,1,0,0,0],
+                e: [0,0,1,0,0,0],
+                f: [1,1,0,0,0,0]
+              }
+            },
+    boards: { own: 
+              { a: [1,1,0,0,0,0],
+                b: [0,1,1,0,0,0],
+                c: [0,0,1,0,0,0],
+                d: [0,0,1,0,0,0],
+                e: [0,0,0,1,1,0],
+                f: [0,0,0,0,1,1]
+              },
+            opponent:
+              { a: [0,0,0,0,1,1],
+                b: [0,0,0,1,1,0],
+                c: [0,0,1,0,0,0],
+                d: [0,0,1,0,0,0],
+                e: [0,1,1,0,0,0],
+                f: [1,1,0,0,0,0]
               }
             },
     ships:  { own:
-                [ { row: 'a', col: 1, size: 2, sunk: false, horizontal: true},
-                  { row: 'b', col: 2, size: 2, sunk: false, horizontal: true},
-                  { row: 'c', col: 3, size: 2, sunk: false, horizontal: false},
-                  { row: 'e', col: 4, size: 2, sunk: false, horizontal: true},
-                  { row: 'f', col: 5, size: 2, sunk: false, horizontal: true},
+                [ { row: 'a', col: 1, size: 2, sunk: true, hit: true, horizontal: true},
+                  { row: 'b', col: 2, size: 2, sunk: false, hit: true, horizontal: true},
+                  { row: 'c', col: 3, size: 2, sunk: false, hit: false, horizontal: false},
+                  { row: 'e', col: 4, size: 2, sunk: false, hit: false, horizontal: true},
+                  { row: 'f', col: 5, size: 2, sunk: false, hit: false, horizontal: true},
                 ],
               opponent:
-                [ { row: 'a', col: 4, size: 2, sunk: false, horizontal: true},
-                  { row: 'b', col: 3, size: 2, sunk: false, horizontal: true},
-                  { row: 'c', col: 3, size: 2, sunk: false, horizontal: false},
-                  { row: 'e', col: 2, size: 2, sunk: false, horizontal: true},
-                  { row: 'f', col: 1, size: 2, sunk: false, horizontal: true},
+                [ { row: 'a', col: 5, size: 2, sunk: false, hit: false, horizontal: true},
+                  { row: 'b', col: 4, size: 2, sunk: false, hit: false, horizontal: true},
+                  { row: 'c', col: 3, size: 2, sunk: false, hit: true, horizontal: false},
+                  { row: 'e', col: 2, size: 2, sunk: false, hit: true, horizontal: true},
+                  { row: 'f', col: 1, size: 2, sunk: true, hit: true, horizontal: true},
                 ]
             },
     turn: { player: 'server',
             row: '',
             col: ''
           }
-        }
+      }
 }
