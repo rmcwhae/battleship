9bb7916b63cd2bbe0b345aa324543597c758a41c
Russell McWhae <russell.mcwhae@gmail.com>
2019-10-05 12:03:34 -0600

    Correct player-board animations now shown

--- src/components/GameContainer.js
+++ src/components/GameContainer.js
@@ -10,25 +10,39 @@ export default function GameContainer(props) {
 
   React.useEffect(() => {
     // console.log("Before game:", props);
-    setGame(new Game({ appState: props.state, sentGame: props.sentGame, setScene: props.setScene, gameOver: props.gameOver }));
+    setGame(
+      new Game({
+        appState: props.state,
+        sentGame: props.sentGame,
+        setScene: props.setScene,
+        gameOver: props.gameOver
+      })
+    );
   }, []);
 
   React.useEffect(() => {
     game && game.setProps(props);
     if (bootScene) {
-      console.log("Checking scene in Container:", props);
-      console.log("Game is now ->", props.state.gameState);
-      bootScene.displayGrid
-      (50, 80, props.state.gameState.boards.own, props.state.gameState.shots.own, false);
-      bootScene.renderShips('playerBoard', props.state.gameState.ships.own, false, false);
+      console.log('Checking scene in Container:', props);
+      console.log('Game is now ->', props.state.gameState);
+      bootScene.displayGrid(
+        50,
+        80,
+        props.state.gameState.boards.own,
+        props.state.gameState.shots.own,
+        false
+      );
+      bootScene.renderShips(
+        'playerBoard',
+        props.state.gameState.ships.own,
+        false,
+        false
+      );
+      bootScene.explode('playerBoard', props.state.gameState.turn.shot.row, props.state.gameState.turn.shot.col, !props.state.gameState.turn.shot.hit); // give animation of red or blue explosion
     }
-  
   }, [props.state.gameState]);
 
   bootScene = game && game.scene.getScene('Boot');
 
- 
-  return (
-    <section className="phaser-container" id="battleship" />
-  );
+  return <section className="phaser-container" id="battleship" />;
 }
